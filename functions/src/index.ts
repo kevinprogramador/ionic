6da1201a3d319e99682569a27003dd9c4350cb82
Felix O <felix.o.stack@gmail.com>
2020-02-21 13:37:19 -0800

    Updated index.ts
    Initial admin setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,25 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+// Interfaces
+
+interface User {
+    createdAt?: string;
+    customerId?: any;
+    disabled?: boolean;
+    displayName?: string;
+    email?: string;
+    emailVerified?: boolean;
+    notifications?: object;
+    photoURL?: string;
+    source?: string;
+    token?: string;
+    uid: string;
+}
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
